7d0385d017d2ba9d33f3fb54c246f5b4261ccf2d
sersoysem <120250940+sersoysem@users.noreply.github.com>
2023-11-08 20:02:43 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace deneme2
+{
+    public partial class Form1 : Form
+    {
+        
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-UK4AJ54Q;Initial Catalog=kullanicilar;Integrated Security=True");
+        private int girisDenemesi = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                string sql = "select * from parola where Ad= @adi AND Sifre= @sifresi ";
+                SqlParameter prm1 = new SqlParameter("adi", textBox1.Text.Trim());
+                SqlParameter prm2 = new SqlParameter("sifresi", textBox2.Text.Trim());
+                SqlCommand komut = new SqlCommand(sql, baglanti);
+                komut.Parameters.Add(prm1);
+                komut.Parameters.Add(prm2);
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(komut);
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    Form2 fr = new Form2();
+                    fr.Show();
+                }
+                else
+                {
+                    girisDenemesi--; // Her hata durumunda deneme hakkını azalt
+                    MessageBox.Show("Kullanıcı adı veya şifreniz hatalı. Kalan Deneme Hakkı: " + girisDenemesi);
+
+                    if (girisDenemesi == 0)
+                    {
+                        button1.Enabled = false; // Deneme hakkı tükendiğinde giriş düğmesini devre dışı bırak
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                
+
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form3 fr1 = new Form3();
+            fr1.Show();
+
+        }
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace deneme2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-UK4AJ54Q;Initial Catalog=kullanicilar;Integrated Security=True");
+        private void veriler()
+        {
+            //Admin kısmına liste ekleyip verileri görüntüleyebilmek için;
+            /*listView1.Items.Clear();
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * From parola", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["id"].ToString();
+                ekle.SubItems.Add(oku["Ad"].ToString());
+                ekle.SubItems.Add(oku["Sifre"].ToString());
+                listView1.Items.Add(ekle);
+            }
+            baglanti.Close();*/
+        }
+        int id = 0;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("update parola set Sifre="+ textBox2.Text.ToString()+"where id= "+ textBox1.Text.ToString(),baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            veriler();
+            MessageBox.Show("Şifreniz başarıyla değiştirildi.");
+        }
+
+        /*private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            id = int.Parse(listView1.SelectedItems[0].SubItems[0].Text);
+            textBox1.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            textBox2.Text = listView1.SelectedItems[0].SubItems[2].Text;
+        }*/
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            veriler();
+        }
+    }
+}
